02dbac789b266f47745364d83f0203617cf73d03
sahilkumar12-max <68015118+sahilkumar12-max@users.noreply.github.com>
2024-01-27 20:50:52 +0530

    Add project files.

---
+++ Weather App/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace Weather_App
+{
+    partial class btn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(btn));
+            this.lblcity = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbcity = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblCon = new System.Windows.Forms.Label();
+            this.lblDetail = new System.Windows.Forms.Label();
+            this.lblSrise = new System.Windows.Forms.Label();
+            this.lblSset = new System.Windows.Forms.Label();
+            this.lblWind = new System.Windows.Forms.Label();
+            this.lblH = new System.Windows.Forms.Label();
+            this.lblP = new System.Windows.Forms.Label();
+            this.lblWV = new System.Windows.Forms.Label();
+            this.lblSsetV = new System.Windows.Forms.Label();
+            this.lblSriseV = new System.Windows.Forms.Label();
+            this.lblHV = new System.Windows.Forms.Label();
+            this.lblPV = new System.Windows.Forms.Label();
+            this.picICON = new System.Windows.Forms.PictureBox();
+            this.lblDetailV = new System.Windows.Forms.Label();
+            this.lblConV = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.lblMaxV = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picICON)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblcity
+            // 
+            this.lblcity.AutoSize = true;
+            this.lblcity.BackColor = System.Drawing.Color.Transparent;
+            this.lblcity.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcity.Location = new System.Drawing.Point(70, 63);
+            this.lblcity.Name = "lblcity";
+            this.lblcity.Size = new System.Drawing.Size(43, 15);
+            this.lblcity.TabIndex = 0;
+            this.lblcity.Text = "City";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // tbcity
+            // 
+            this.tbcity.Location = new System.Drawing.Point(119, 63);
+            this.tbcity.Name = "tbcity";
+            this.tbcity.Size = new System.Drawing.Size(130, 20);
+            this.tbcity.TabIndex = 2;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.Transparent;
+            this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(255, 60);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(64, 23);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "SEARCH";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblCon
+            // 
+            this.lblCon.AutoSize = true;
+            this.lblCon.BackColor = System.Drawing.Color.Transparent;
+            this.lblCon.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCon.Location = new System.Drawing.Point(70, 195);
+            this.lblCon.Name = "lblCon";
+            this.lblCon.Size = new System.Drawing.Size(88, 15);
+            this.lblCon.TabIndex = 4;
+            this.lblCon.Text = "Condition";
+            // 
+            // lblDetail
+            // 
+            this.lblDetail.AutoSize = true;
+            this.lblDetail.BackColor = System.Drawing.Color.Transparent;
+            this.lblDetail.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDetail.Location = new System.Drawing.Point(70, 236);
+            this.lblDetail.Name = "lblDetail";
+            this.lblDetail.Size = new System.Drawing.Size(61, 15);
+            this.lblDetail.TabIndex = 5;
+            this.lblDetail.Text = "Detail";
+            // 
+            // lblSrise
+            // 
+            this.lblSrise.AutoSize = true;
+            this.lblSrise.BackColor = System.Drawing.Color.Transparent;
+            this.lblSrise.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSrise.Location = new System.Drawing.Point(70, 275);
+            this.lblSrise.Name = "lblSrise";
+            this.lblSrise.Size = new System.Drawing.Size(70, 15);
+            this.lblSrise.TabIndex = 6;
+            this.lblSrise.Text = "Sunrise";
+            // 
+            // lblSset
+            // 
+            this.lblSset.AutoSize = true;
+            this.lblSset.BackColor = System.Drawing.Color.Transparent;
+            this.lblSset.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSset.Location = new System.Drawing.Point(70, 312);
+            this.lblSset.Name = "lblSset";
+            this.lblSset.Size = new System.Drawing.Size(61, 15);
+            this.lblSset.TabIndex = 7;
+            this.lblSset.Text = "Sunset";
+            // 
+            // lblWind
+            // 
+            this.lblWind.AutoSize = true;
+            this.lblWind.BackColor = System.Drawing.Color.Transparent;
+            this.lblWind.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWind.Location = new System.Drawing.Point(320, 195);
+            this.lblWind.Name = "lblWind";
+            this.lblWind.Size = new System.Drawing.Size(97, 15);
+            this.lblWind.TabIndex = 8;
+            this.lblWind.Text = "Wind Speed";
+            // 
+            // lblH
+            // 
+            this.lblH.AutoSize = true;
+            this.lblH.BackColor = System.Drawing.Color.Transparent;
+            this.lblH.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblH.Location = new System.Drawing.Point(320, 236);
+            this.lblH.Name = "lblH";
+            this.lblH.Size = new System.Drawing.Size(79, 15);
+            this.lblH.TabIndex = 9;
+            this.lblH.Text = "Humidity";
+            // 
+            // lblP
+            // 
+            this.lblP.AutoSize = true;
+            this.lblP.BackColor = System.Drawing.Color.Transparent;
+            this.lblP.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblP.Location = new System.Drawing.Point(320, 275);
+            this.lblP.Name = "lblP";
+            this.lblP.Size = new System.Drawing.Size(79, 15);
+            this.lblP.TabIndex = 10;
+            this.lblP.Text = "Pressure";
+            // 
+            // lblWV
+            // 
+            this.lblWV.AutoSize = true;
+            this.lblWV.BackColor = System.Drawing.Color.Transparent;
+            this.lblWV.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWV.Location = new System.Drawing.Point(436, 275);
+            this.lblWV.Name = "lblWV";
+            this.lblWV.Size = new System.Drawing.Size(34, 15);
+            this.lblWV.TabIndex = 11;
+            this.lblWV.Text = "N/A";
+            // 
+            // lblSsetV
+            // 
+            this.lblSsetV.AutoSize = true;
+            this.lblSsetV.BackColor = System.Drawing.Color.Transparent;
+            this.lblSsetV.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSsetV.Location = new System.Drawing.Point(175, 275);
+            this.lblSsetV.Name = "lblSsetV";
+            this.lblSsetV.Size = new System.Drawing.Size(34, 15);
+            this.lblSsetV.TabIndex = 12;
+            this.lblSsetV.Text = "N/A";
+            // 
+            // lblSriseV
+            // 
+            this.lblSriseV.AutoSize = true;
+            this.lblSriseV.BackColor = System.Drawing.Color.Transparent;
+            this.lblSriseV.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSriseV.Location = new System.Drawing.Point(175, 236);
+            this.lblSriseV.Name = "lblSriseV";
+            this.lblSriseV.Size = new System.Drawing.Size(34, 15);
+            this.lblSriseV.TabIndex = 13;
+            this.lblSriseV.Text = "N/A";
+            // 
+            // lblHV
+            // 
+            this.lblHV.AutoSize = true;
+            this.lblHV.BackColor = System.Drawing.Color.Transparent;
+            this.lblHV.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHV.Location = new System.Drawing.Point(436, 195);
+            this.lblHV.Name = "lblHV";
+            this.lblHV.Size = new System.Drawing.Size(34, 15);
+            this.lblHV.TabIndex = 14;
+            this.lblHV.Text = "N/A";
+            // 
+            // lblPV
+            // 
+            this.lblPV.AutoSize = true;
+            this.lblPV.BackColor = System.Drawing.Color.Transparent;
+            this.lblPV.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPV.Location = new System.Drawing.Point(436, 236);
+            this.lblPV.Name = "lblPV";
+            this.lblPV.Size = new System.Drawing.Size(34, 15);
+            this.lblPV.TabIndex = 15;
+            this.lblPV.Text = "N/A";
+            // 
+            // picICON
+            // 
+            this.picICON.BackColor = System.Drawing.Color.Transparent;
+            this.picICON.Location = new System.Drawing.Point(73, 124);
+            this.picICON.Name = "picICON";
+            this.picICON.Size = new System.Drawing.Size(67, 50);
+            this.picICON.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picICON.TabIndex = 16;
+            this.picICON.TabStop = false;
+            // 
+            // lblDetailV
+            // 
+            this.lblDetailV.AutoSize = true;
+            this.lblDetailV.BackColor = System.Drawing.Color.Transparent;
+            this.lblDetailV.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDetailV.Location = new System.Drawing.Point(175, 195);
+            this.lblDetailV.Name = "lblDetailV";
+            this.lblDetailV.Size = new System.Drawing.Size(34, 15);
+            this.lblDetailV.TabIndex = 17;
+            this.lblDetailV.Text = "N/A";
+            // 
+            // lblConV
+            // 
+            this.lblConV.AutoSize = true;
+            this.lblConV.BackColor = System.Drawing.Color.Transparent;
+            this.lblConV.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConV.Location = new System.Drawing.Point(175, 312);
+            this.lblConV.Name = "lblConV";
+            this.lblConV.Size = new System.Drawing.Size(34, 15);
+            this.lblConV.TabIndex = 18;
+            this.lblConV.Text = "N/A";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft YaHei", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Black;
+            this.lblTitle.Location = new System.Drawing.Point(210, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(142, 22);
+            this.lblTitle.TabIndex = 19;
+            this.lblTitle.Text = "Weather Forcast";
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.BackColor = System.Drawing.Color.Transparent;
+            this.lblMax.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMax.Location = new System.Drawing.Point(320, 312);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(88, 15);
+            this.lblMax.TabIndex = 20;
+            this.lblMax.Text = "Temp Max ";
+            // 
+            // lblMaxV
+            // 
+            this.lblMaxV.AutoSize = true;
+            this.lblMaxV.BackColor = System.Drawing.Color.Transparent;
+            this.lblMaxV.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaxV.Location = new System.Drawing.Point(436, 312);
+            this.lblMaxV.Name = "lblMaxV";
+            this.lblMaxV.Size = new System.Drawing.Size(34, 15);
+            this.lblMaxV.TabIndex = 22;
+            this.lblMaxV.Text = "N/A";
+            // 
+            // btn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(589, 510);
+            this.Controls.Add(this.lblMaxV);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblConV);
+            this.Controls.Add(this.lblDetailV);
+            this.Controls.Add(this.picICON);
+            this.Controls.Add(this.lblPV);
+            this.Controls.Add(this.lblHV);
+            this.Controls.Add(this.lblSriseV);
+            this.Controls.Add(this.lblSsetV);
+            this.Controls.Add(this.lblWV);
+            this.Controls.Add(this.lblP);
+            this.Controls.Add(this.lblH);
+            this.Controls.Add(this.lblWind);
+            this.Controls.Add(this.lblSset);
+            this.Controls.Add(this.lblSrise);
+            this.Controls.Add(this.lblDetail);
+            this.Controls.Add(this.lblCon);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.tbcity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblcity);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.BurlyWood;
+            this.Name = "btn";
+            this.Text = "form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picICON)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblcity;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbcity;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblCon;
+        private System.Windows.Forms.Label lblDetail;
+        private System.Windows.Forms.Label lblSrise;
+        private System.Windows.Forms.Label lblSset;
+        private System.Windows.Forms.Label lblWind;
+        private System.Windows.Forms.Label lblH;
+        private System.Windows.Forms.Label lblP;
+        private System.Windows.Forms.Label lblWV;
+        private System.Windows.Forms.Label lblSsetV;
+        private System.Windows.Forms.Label lblSriseV;
+        private System.Windows.Forms.Label lblHV;
+        private System.Windows.Forms.Label lblPV;
+        private System.Windows.Forms.PictureBox picICON;
+        private System.Windows.Forms.Label lblDetailV;
+        private System.Windows.Forms.Label lblConV;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.Label lblMaxV;
+    }
+}
+
---
+++ Weather App/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Weather_App
+{
+    public partial class btn : Form
+    {
+        public btn()
+        {
+            InitializeComponent();
+        }
+        string APIKEY = "9828486cda8a1ebce9c0f6dc54d16a78";
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            getWeather();
+        }
+        void getWeather()
+        {
+            using (WebClient web = new WebClient())
+            {
+                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", tbcity.Text, APIKEY);
+                var json = web.DownloadString(url);
+                weatherinfo.root Info = JsonConvert.DeserializeObject<weatherinfo.root>(json);
+
+                picICON.ImageLocation = "https://openweathermap.org/img/w/" + Info.weather[0].icon + ".png";
+                lblMaxV.Text = Info.main.temp_max.ToString();
+                lblConV.Text = Info.weather[0].main;
+                lblDetailV.Text = Info.weather[0].description;
+                lblSriseV.Text = Info.sys.sunrise.ToString();
+                lblSsetV.Text = Info.sys.sunset.ToString();
+                lblWV.Text = Info.wind.speed.ToString();
+                lblHV.Text = Info.main.humidity.ToString();
+                lblPV.Text = Info.main.pressure.ToString();
+
+            }
+        }
+    }
+}
